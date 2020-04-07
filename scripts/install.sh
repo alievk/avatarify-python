46bcae385f959fcf8f65d0bbe616b25d2dcee437
Ali Aliev <alien-box@yandex.ru>
2020-04-07 13:13:11 +0300

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,5 +14,6 @@ git clone https://github.com/umlaeute/v4l2loopback
 cd v4l2loopback
 make && sudo make install
 sudo depmod -a
-sudo insmod v4l2loopback.ko exclusive_caps=1 video_nr=1 card_label="avatarify"
+#sudo insmod v4l2loopback.ko exclusive_caps=1 video_nr=1 card_label="avatarify"
+sudo modprobe v4l2loopback exclusive_caps=1 video_nr=1 card_label="avatarify"
 cd ..
