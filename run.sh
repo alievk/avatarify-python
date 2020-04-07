875fd614c20e9ec025312d61a7ff9a5d92ecdd70
Ali Aliev <alien-box@yandex.ru>
2020-04-07 13:12:32 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ CKPT=vox-adv-cpk.pth.tar
 CAMID=0
 CAMID_VIRT=99
 
-PYTHONPATH=$PYTHONPATH:fomm
+export PYTHONPATH=$PYTHONPATH:fomm
 
 FFCMD='ffmpeg -y -i pipe:0 tmp.mp4'
 FFCMD='ffmpeg -re -i pipe:0 -vf format=pix_fmts=yuv420p -f v4l2 /dev/video1'
