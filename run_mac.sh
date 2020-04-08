6444026180bd52c323f0a394a6c3302437973d18
Karim Iskakov <kar.iskakov@gmail.com>
2020-04-08 23:17:15 +0300

    Change default mac CAMID

--- run_mac.sh
+++ run_mac.sh
@@ -1,6 +1,6 @@
 CONFIG=fomm/config/vox-adv-256.yaml
 CKPT=vox-adv-cpk.pth.tar
-CAMID=0
+CAMID=2
 
 export PYTHONPATH=$PYTHONPATH:fomm
 python cam_fomm.py --config $CONFIG --checkpoint $CKPT --cam $CAMID --relative --adapt_scale --no-pad
