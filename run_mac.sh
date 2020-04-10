8ab8a8777d2a7bfe25a235406f066b86f9a301b3
Ali Aliev <alievk91@gmail.com>
2020-04-10 14:52:37 +0300

    fix run_mac.sh

--- run_mac.sh
+++ run_mac.sh
@@ -1,6 +1,7 @@
+source scripts/settings.sh
+
 CONFIG=fomm/config/vox-adv-256.yaml
 CKPT=vox-adv-cpk.pth.tar
-CAMID=2
 
 export PYTHONPATH=$PYTHONPATH:fomm
 python cam_fomm.py --config $CONFIG --checkpoint $CKPT --cam $CAMID --relative --adapt_scale --no-pad
