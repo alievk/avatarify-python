00f3e05173e6cd9c2eaa84dc904ac8da3f616a02
Ali Aliev <alievk91@gmail.com>
2020-04-11 18:10:53 +0300

    install v4l2loopback from apt

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,9 @@
 
 source scripts/settings.sh
 
+# v4l2loopback
+sudo apt install v4l2loopback-dkms
+
 source $(conda info --base)/etc/profile.d/conda.sh
 conda create -y -n $CONDA_ENV_NAME python=3.8
 conda activate $CONDA_ENV_NAME
@@ -9,12 +12,3 @@ conda activate $CONDA_ENV_NAME
 # FOMM
 git clone https://github.com/alievk/first-order-model.git fomm
 pip install -r fomm/requirements.txt
-
-# v4l2loopback
-git clone https://github.com/umlaeute/v4l2loopback
-cd v4l2loopback
-make && sudo make install
-sudo depmod -a
-#sudo insmod v4l2loopback.ko exclusive_caps=1 video_nr=1 card_label="avatarify"
-sudo modprobe v4l2loopback exclusive_caps=1 video_nr=$CAMID_VIRT card_label="avatarify"
-cd ..
