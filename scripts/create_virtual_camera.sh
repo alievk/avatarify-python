853e5fb3faee337f4b94382203617e0b98434d7c
Ali Aliev <alievk91@gmail.com>
2020-04-14 22:34:24 +0300

    check device is created

--- scripts/create_virtual_camera.sh
+++ scripts/create_virtual_camera.sh
@@ -2,5 +2,8 @@
 
 source scripts/settings.sh
 
-echo 'Creating virtual camera (this may require sudo password once after reboot)'
-sudo modprobe v4l2loopback exclusive_caps=1 video_nr=$CAMID_VIRT card_label="avatarify"
+FILE=/dev/video$CAMID_VIRT
+if [[ ! -w "$FILE" ]]; then
+    echo "Creating virtual camera $FILE (sudo privelege required)"
+    sudo modprobe v4l2loopback exclusive_caps=1 video_nr=$CAMID_VIRT card_label="avatarify"
+fi
