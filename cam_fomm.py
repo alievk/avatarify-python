00a090579aae59cf5fdb864808c3c7478100d237
Ali Aliev <alievk91@gmail.com>
2020-04-15 15:20:31 +0300

    fix avatar switching

--- cam_fomm.py
+++ cam_fomm.py
@@ -159,8 +159,9 @@ def load_stylegan_avatar():
     return image
 
 def change_avatar(fa, new_avatar):
-    global avatar_kp    
+    global avatar, avatar_kp
     avatar_kp = get_frame_kp(fa, new_avatar)
+    avatar = new_avatar
 
 def log(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
@@ -230,6 +231,7 @@ if __name__ == "__main__":
         stream = pyfakewebcam.FakeWebcam(f'/dev/video{opt.virt_cam}', frame.shape[1], frame.shape[0])
 
     cur_ava = 0    
+    avatar = None
     change_avatar(fa, avatars[cur_ava])
     passthrough = False
 
@@ -259,7 +261,7 @@ if __name__ == "__main__":
         frame = resize(frame, (256, 256))[..., :3]
 
         if find_keyframe:
-            if is_new_frame_better(fa, avatars[cur_ava], frame, device):
+            if is_new_frame_better(fa, avatar, frame, device):
                 log("Taking new frame!")
                 green_overlay = True
                 kp_driving_initial = None
@@ -268,7 +270,7 @@ if __name__ == "__main__":
             out = frame
         else:
             pred_start = time.time()
-            pred = predict(frame, avatars[cur_ava], opt.relative, opt.adapt_scale, fa, device=device)
+            pred = predict(frame, avatar, opt.relative, opt.adapt_scale, fa, device=device)
             out = pred
             pred_time = (time.time() - pred_start) * 1000
             if opt.debug:
@@ -315,12 +317,13 @@ if __name__ == "__main__":
         elif key == ord('f'):
             find_keyframe = not find_keyframe
         elif key == ord('q'):
-            if opt.debug:
-                log(f'Loading StyleGAN avatar...')
-            avatar = load_stylegan_avatar()
-
-            passthrough = False
-            change_avatar(fa, avatar)
+            try:
+                log('Loading StyleGAN avatar...')
+                avatar = load_stylegan_avatar()
+                passthrough = False
+                change_avatar(fa, avatar)
+            except:
+                log('Failed to load StyleGAN avatar')
         elif 48 < key < 58:
             cur_ava = min(key - 49, len(avatars) - 1)
             passthrough = False
