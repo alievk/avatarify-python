034d4b3c473d921d31cdc5eb833dcfe209f1d400
Ali Aliev <alievk91@gmail.com>
2020-04-16 11:34:11 +0300

    build from source

--- scripts/create_virtual_camera.sh
+++ scripts/create_virtual_camera.sh
@@ -6,4 +6,5 @@ FILE=/dev/video$CAMID_VIRT
 if [[ ! -w "$FILE" ]]; then
     echo "Creating virtual camera $FILE (sudo privelege required)"
     sudo modprobe v4l2loopback exclusive_caps=1 video_nr=$CAMID_VIRT card_label="avatarify"
+    #sudo v4l2-ctl -d /dev/video$CAMID_VIRT -c timeout=1000
 fi
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,14 @@
 source scripts/settings.sh
 
 # v4l2loopback
-sudo apt install v4l2loopback-dkms
+#sudo apt install v4l2loopback-dkms
+#sudo apt install v4l-utils
+
+git clone https://github.com/alievk/v4l2loopback.git
+cd v4l2loopback
+make && sudo make install
+sudo depmod -a
+cd ..
 
 source $(conda info --base)/etc/profile.d/conda.sh
 conda create -y -n $CONDA_ENV_NAME python=3.8
