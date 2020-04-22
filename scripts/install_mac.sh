dac7bbf131cae5349fe9fc77fdbf9b251c04d8c9
Karim Iskakov <karfly@yandex.ru>
2020-04-22 14:12:42 +0300

    Fix mac install

--- scripts/install_mac.sh
+++ scripts/install_mac.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-ENV_NAME=avatarify
-
-conda create -y -n $ENV_NAME python=3.7
+source scripts/settings.sh
 
 source $(conda info --base)/etc/profile.d/conda.sh
-conda activate $ENV_NAME
+conda create -y -n $CONDA_ENV_NAME python=3.7
+conda activate $CONDA_ENV_NAME
 
 # FOMM
 rm -rf fomm 2> /dev/null
 git clone https://github.com/alievk/first-order-model.git fomm
 
+conda install -y pytorch==1.0.0 torchvision==0.2.1 -c pytorch
+conda install -y matplotlib==2.2.2
 pip install -r requirements.txt
