24d66ae735d93cfe29cb1a6fb92455c3bf72eea1
Ali Aliev <alievk91@gmail.com>
2020-04-27 16:05:48 +0300

    add check for prerequisites (lunix & mac)

--- scripts/install_mac.sh
+++ scripts/install_mac.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# check prerequisites
+command -v conda >/dev/null 2>&1 || { echo >&2 "conda not found. Please refer to the README and install Miniconda."; exit 1; }
+command -v git >/dev/null 2>&1 || { echo >&2 "git not found. Please refer to the README and install Git."; exit 1; }
+
 source scripts/settings.sh
 
 source $(conda info --base)/etc/profile.d/conda.sh
