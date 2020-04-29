f7bfac9e5e4ec1ff01e5098e0f1fe2432d126956
Ali Aliev <alievk91@gmail.com>
2020-04-29 18:38:56 +0300

    pass extra args

--- run_windows.bat
+++ run_windows.bat
@@ -7,4 +7,4 @@ call conda activate %CONDA_ENV_NAME%
 set CONFIG=fomm/config/vox-adv-256.yaml
 
 set PYTHONPATH=%PYTHONPATH%;%CD%/fomm
-call python cam_fomm.py --config %CONFIG% --cam %CAMID% --relative --adapt_scale --no-pad --checkpoint vox-adv-cpk.pth.tar
+call python cam_fomm.py --config %CONFIG% --cam %CAMID% --relative --adapt_scale --no-pad --checkpoint vox-adv-cpk.pth.tar %*
