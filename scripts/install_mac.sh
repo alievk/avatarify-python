36af2a96d89f00d53dfe31d52fb2332b068cbaae
Ali Aliev <alievk91@gmail.com>
2020-05-02 16:37:58 +0300

    fix blosc version

--- scripts/install_mac.sh
+++ scripts/install_mac.sh
@@ -15,5 +15,6 @@ rm -rf fomm 2> /dev/null
 git clone https://github.com/alievk/first-order-model.git fomm
 
 conda install -y pytorch==1.0.0 torchvision==0.2.1 -c pytorch
+conda install -y python-blosc==1.7.0 -c conda-forge
 conda install -y matplotlib==2.2.2
 pip install -r requirements.txt
