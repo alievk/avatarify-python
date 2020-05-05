161878f51f2e58d77e8541cec8aae884beb07422
Ali Aliev <alievk91@gmail.com>
2020-05-05 14:42:20 +0300

    faster send/recv

---
+++ afy/networking.py
@@ -0,0 +1,104 @@
+import zmq
+import numpy as np
+
+
+class SerializingSocket(zmq.Socket):
+    """Numpy array serialization methods.
+
+    Modelled on PyZMQ serialization examples.
+
+    Used for sending / receiving OpenCV images, which are Numpy arrays.
+    Also used for sending / receiving jpg compressed OpenCV images.
+    """
+
+    def send_array(self, A, msg='NoName', flags=0, copy=True, track=False):
+        """Sends a numpy array with metadata and text message.
+
+        Sends a numpy array with the metadata necessary for reconstructing
+        the array (dtype,shape). Also sends a text msg, often the array or
+        image name.
+
+        Arguments:
+          A: numpy array or OpenCV image.
+          msg: (optional) array name, image name or text message.
+          flags: (optional) zmq flags.
+          copy: (optional) zmq copy flag.
+          track: (optional) zmq track flag.
+        """
+
+        md = dict(
+            msg=msg,
+            dtype=str(A.dtype),
+            shape=A.shape,
+        )
+        self.send_json(md, flags | zmq.SNDMORE)
+        return self.send(A, flags, copy=copy, track=track)
+
+    def send_data(self,
+                 msg='NoName',
+                 data=b'00',
+                 flags=0,
+                 copy=True,
+                 track=False):
+        """Send a jpg buffer with a text message.
+
+        Sends a jpg bytestring of an OpenCV image.
+        Also sends text msg, often the image name.
+
+        Arguments:
+          msg: image name or text message.
+          data: binary data to be sent.
+          flags: (optional) zmq flags.
+          copy: (optional) zmq copy flag.
+          track: (optional) zmq track flag.
+        """
+
+        md = dict(msg=msg, )
+        self.send_json(md, flags | zmq.SNDMORE)
+        return self.send(data, flags, copy=copy, track=track)
+
+    def recv_array(self, flags=0, copy=True, track=False):
+        """Receives a numpy array with metadata and text message.
+
+        Receives a numpy array with the metadata necessary
+        for reconstructing the array (dtype,shape).
+        Returns the array and a text msg, often the array or image name.
+
+        Arguments:
+          flags: (optional) zmq flags.
+          copy: (optional) zmq copy flag.
+          track: (optional) zmq track flag.
+
+        Returns:
+          msg: image name or text message.
+          A: numpy array or OpenCV image reconstructed with dtype and shape.
+        """
+
+        md = self.recv_json(flags=flags)
+        msg = self.recv(flags=flags, copy=copy, track=track)
+        A = np.frombuffer(msg, dtype=md['dtype'])
+        return (md['msg'], A.reshape(md['shape']))
+
+    def recv_data(self, flags=0, copy=True, track=False):
+        """Receives a jpg buffer and a text msg.
+
+        Receives a jpg bytestring of an OpenCV image.
+        Also receives a text msg, often the image name.
+
+        Arguments:
+          flags: (optional) zmq flags.
+          copy: (optional) zmq copy flag.
+          track: (optional) zmq track flag.
+
+        Returns:
+          msg: image name or text message.
+          data: bytestring, containing data.
+        """
+
+        md = self.recv_json(flags=flags)  # metadata text
+        data = self.recv(flags=flags, copy=copy, track=track)
+        return (md['msg'], data)
+
+
+class SerializingContext(zmq.Context):
+    _socket_class = SerializingSocket
--- afy/predictor_remote.py
+++ afy/predictor_remote.py
@@ -1,38 +1,34 @@
 from predictor_local import PredictorLocal
 from arguments import opt
+from networking import SerializingContext
 
 from time import time
 
+import cv2
+import numpy as np
 import zmq
-import imagezmq
-import blosc
 import msgpack
 import msgpack_numpy as m
 m.patch()
 
 
 DEFAULT_PORT = 5556
+JPEG_QUALITY = 95
 
 
-if opt.compress:
-    def pack_message(msg):
-        return blosc.compress(msgpack.packb(msg), typesize=8)
-    
-    def unpack_message(msg):
-        return msgpack.unpackb(blosc.decompress(msg))
-else:
-    def pack_message(msg):
-        return msgpack.packb(msg)
-    
-    def unpack_message(msg):
-        return msgpack.unpackb(msg)
+def pack_message(msg):
+    return msgpack.packb(msg)
+
+
+def unpack_message(msg):
+    return msgpack.unpackb(msg)
 
 
 class PredictorRemote:
     def __init__(self, *args, worker_host='localhost', worker_port=DEFAULT_PORT, **kwargs):
         self.worker_host = worker_host
         self.worker_port = worker_port
-        self.context = zmq.Context()
+        self.context = SerializingContext()
         self.socket = self.context.socket(zmq.PAIR)
         self.socket.connect(f"tcp://{worker_host}:{worker_port}")
         print(f"Connected to {worker_host}:{worker_port}")
@@ -46,67 +42,98 @@ class PredictorRemote:
         )
         return self._send_recv_msg(msg)
 
-    def __getattr__(self, item):
-        return lambda *args, **kwargs: self._send_recv_msg((item, args, kwargs))
+    def __getattr__(self, attr):
+        return lambda *args, **kwargs: self._send_recv_msg((attr, args, kwargs))
 
     def _send_recv_msg(self, msg):
-        s = time()
-        msg_pack = pack_message(msg)
-        print('PACK ', int((time() - s)*1000))
-        s = time()
-        self.socket.send(msg_pack, copy=True)
-        print('SEND ', int((time() - s)*1000))
-        s = time()
-        response = self.socket.recv()
-        print('RECV ', int((time() - s)*1000))
-        s = time()
-        msg_recv = unpack_message(response)
-        print('UNPACK ', int((time() - s)*1000))
-        return msg_recv
+        attr, args, kwargs = msg
 
-from time import time
+        s = time()
+        if attr == 'predict':
+            image = args[0]
+            assert isinstance(image, np.ndarray), 'Expected image'
+            print(image.dtype)
+            ret_code, data = cv2.imencode(".jpg", image, [int(cv2.IMWRITE_JPEG_QUALITY), JPEG_QUALITY])
+        else:
+            data = pack_message((args, kwargs))
+        print('PACK ', int((time() - s)*1000))
+
+        s = time()
+        self.socket.send_data(attr, data)
+        print('SEND ', int((time() - s)*1000))
+
+        s = time()
+        attr_recv, data_recv = self.socket.recv_data()
+        print('RECV ', int((time() - s)*1000))
+
+        s = time()
+        if attr_recv == 'predict':
+            result = cv2.imdecode(np.frombuffer(data_recv, dtype='uint8'), -1)
+        else:
+            result = unpack_message(data_recv)
+        print('UNPACK ', int((time() - s)*1000))
+
+        return result
+
 
 def message_handler(port):
     print("Creating socket")
-    context = zmq.Context()
+    context = SerializingContext()
     socket = context.socket(zmq.PAIR)
     socket.bind("tcp://*:%s" % port)
     predictor = None
     predictor_args = ()
 
     print("Listening for messages on port:", port)
-    while True:
-        s = time()
-        ss = time()
-        msg_raw = socket.recv()
-        print('RECV ', int((time() - ss)*1000))
-        try:
+    try:
+        while True:
+            s = time()
             ss = time()
-            msg = unpack_message(msg_raw)
-            print('UNPACK ', int((time() - ss)*1000))
-        except ValueError:
-            print("Invalid Message")
-            continue
-        method = msg[0]
-        if method == "__init__":
-            predictor_args_new = msg[1:]
-            if predictor_args_new == predictor_args:
-                print("Same config as before... reusing previous predictor")
-            else:
-                del predictor
-                predictor_args = predictor_args_new
-                predictor = PredictorLocal(*predictor_args[0], **predictor_args[1])
-                print("Initialized predictor with:", predictor_args)
-            result = True
-        else:
-            ss = time()
-            result = getattr(predictor, method)(*msg[1], **msg[2])
-            print('CALL ', int((time() - ss)*1000))
-        ss = time()
-        socket.send(pack_message(result), copy=True)
-        print('SEND ', int((time() - ss)*1000))
-        print('CYCLE ', int((time() - s)*1000))
+            attr, data = socket.recv_data()
+            print('RECV ', int((time() - ss)*1000))
 
+            try:
+                ss = time()
+                if attr == 'predict':
+                    image = cv2.imdecode(np.frombuffer(data, dtype='uint8'), -1)
+                else:
+                    args = unpack_message(data)
+                print('UNPACK ', int((time() - ss)*1000))
+            except ValueError:
+                print("Invalid Message")
+                continue
+
+            ss = time()
+            if attr == "__init__":
+                if args == predictor_args:
+                    print("Same config as before... reusing previous predictor")
+                else:
+                    del predictor
+                    predictor_args = args
+                    predictor = PredictorLocal(*predictor_args[0], **predictor_args[1])
+                    print("Initialized predictor with:", predictor_args)
+                result = True
+            elif attr == 'predict':
+                result = getattr(predictor, attr)(image)
+            else:
+                result = getattr(predictor, attr)(*args[0], **args[1])
+            print('CALL ', int((time() - ss)*1000))
+
+            ss = time()
+            if attr == 'predict':
+                assert isinstance(result, np.ndarray), 'Expected image'
+                ret_code, data_send = cv2.imencode(".jpg", result, [int(cv2.IMWRITE_JPEG_QUALITY), JPEG_QUALITY])
+            else:
+                data_send = pack_message(result)
+            print('PACK ', int((time() - ss)*1000))
+
+            ss = time()
+            socket.send_data(attr, data_send)
+            print('SEND ', int((time() - ss)*1000))
+
+            print('CYCLE ', int((time() - s)*1000))
+    except KeyboardInterrupt:
+        pass
 
 def run_worker(port):
     message_handler(port)
