1d60ed3f32fc48a88a2ce72883729eac87f66656
Ali Aliev <alievk91@gmail.com>
2020-05-06 18:48:04 +0300

    fix pythonpath

--- run_mac.sh
+++ run_mac.sh
@@ -6,6 +6,6 @@ conda activate $CONDA_ENV_NAME
 CONFIG=fomm/config/vox-adv-256.yaml
 CKPT=vox-adv-cpk.pth.tar
 
-#export PYTHONPATH=$PYTHONPATH:fomm
+export PYTHONPATH=$PYTHONPATH:$(pwd)
 #export PYTHONPATH=$PYTHONPATH:$(pwd)/fomm
 python afy/cam_fomm.py --config $CONFIG --checkpoint $CKPT --cam $CAMID --relative --adapt_scale --no-pad $@
