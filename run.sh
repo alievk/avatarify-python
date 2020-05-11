d13aa72500e8530aefaba9b567b2914f2a1135f7
Ali Aliev <alievk91@gmail.com>
2020-05-11 21:47:20 +0300

    fix pythonpaths

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ conda activate $CONDA_ENV_NAME
 CONFIG=fomm/config/vox-adv-256.yaml
 CKPT=vox-adv-cpk.pth.tar
 
-export PYTHONPATH=$PYTHONPATH:$(pwd)/fomm
+export PYTHONPATH=$PYTHONPATH:$(pwd):$(pwd)/fomm
 
 python afy/cam_fomm.py --config $CONFIG --checkpoint $CKPT --cam $CAMID --virt-cam $CAMID_VIRT --relative --adapt_scale $@
 
--- run_windows.bat
+++ run_windows.bat
@@ -6,5 +6,5 @@ call conda activate %CONDA_ENV_NAME%
 
 set CONFIG=fomm/config/vox-adv-256.yaml
 
-set PYTHONPATH=%PYTHONPATH%;%CD%/fomm
+set PYTHONPATH=%PYTHONPATH%;%CD%;%CD%/fomm
 call python afy/cam_fomm.py --config %CONFIG% --cam %CAMID% --relative --adapt_scale --no-pad --checkpoint vox-adv-cpk.pth.tar %*
