41998fe5d994d13de060aef93dd536eb94888b9c
Ali Aliev <alievk91@gmail.com>
2020-05-18 18:04:55 +0300

    handle critical request

--- afy/predictor_remote.py
+++ afy/predictor_remote.py
@@ -20,9 +20,9 @@ QUEUE_SIZE = 100
 
 
 class PredictorRemote:
-    def __init__(self, *args, bind_port=None, connect_address=None, **kwargs):
+    def __init__(self, *args, bind_port=None, remote_host=None, **kwargs):
         self.bind_port = bind_port
-        self.connect_address = connect_address
+        self.remote_host = remote_host
         self.predictor_args = (args, kwargs)
         self.timing = AccumDict()
 
@@ -31,26 +31,30 @@ class PredictorRemote:
 
         self.worker_alive = mp.Value('i', 0)
 
-        # if not self.connect_address.startswith("tcp://"):
-        #     self.connect_address = "tcp://" + self.connect_address
+        # if not self.remote_host.startswith("tcp://"):
+        #     self.remote_host = "tcp://" + self.remote_host
 
-        self.send_process = mp.Process(target=self.send_worker, args=(self.connect_address, self.send_queue, self.worker_alive))
-        self.recv_process = mp.Process(target=self.recv_worker, args=(self.connect_address, self.recv_queue, self.worker_alive))
+        self.send_process = mp.Process(target=self.send_worker, args=(self.remote_host, self.send_queue, self.worker_alive))
+        self.recv_process = mp.Process(target=self.recv_worker, args=(self.remote_host, self.recv_queue, self.worker_alive))
 
-        # self.context = SerializingContext()
-        # self.socket = self.context.socket(zmq.PAIR)
+        log("Connecting to remote host {self.remote_host}")
+        self.ctx = SerializingContext()
+        self.sender = self.ctx.socket(zmq.PUSH)
+        self.sender.connect(f"tcp://{remote_host}:5557")
+        self.receiver = self.ctx.socket(zmq.PULL)
+        self.receiver.connect(f"tcp://{remote_host}:5558")
 
         # if self.bind_port is None:
-        #     if not self.connect_address.startswith("tcp://"):
-        #         self.connect_address = "tcp://" + self.connect_address
-        #     log(f"Connecting to {self.connect_address}")
-        #     self.socket.connect(self.connect_address)
+        #     if not self.remote_host.startswith("tcp://"):
+        #         self.remote_host = "tcp://" + self.remote_host
+        #     log(f"Connecting to {self.remote_host}")
+        #     self.socket.connect(self.remote_host)
 
         #     if not check_connection(self.socket):
-        #         self.socket.disconnect(self.connect_address)
-        #         raise ConnectionError(f"Could not connect to {self.connect_address}")
+        #         self.socket.disconnect(self.remote_host)
+        #         raise ConnectionError(f"Could not connect to {self.remote_host}")
 
-        #     log(f"Connected to {self.connect_address}")
+        #     log(f"Connected to {self.remote_host}")
         # else:
         #     self.socket.bind(f"tcp://*:{self.bind_port}")
         #     log(f"Listening on port {self.bind_port}")
@@ -73,45 +77,39 @@ class PredictorRemote:
         self.recv_process.join(timeout=5)
 
     def init_remote_worker(self):
-        return self._remote_call('__init__', self.predictor_args, critical=True)
+        return self._send_recv_async('__init__', self.predictor_args, critical=True)
 
     def __getattr__(self, attr):
         is_critical = attr != 'predict'
-        return lambda *args, **kwargs: self._remote_call(attr, (args, kwargs), critical=is_critical)
+        return lambda *args, **kwargs: self._send_recv_async(attr, (args, kwargs), critical=is_critical)
 
-    def _remote_call(self, method, args, critical=True):
-        msg = {
-            'method': {
-                'name': attr,
-                'critical': critical
-            },
-            'args': args
-        }
-
-        if critical:
-            return self._send_recv_sync(msg)
+    # def _remote_call(self, method, args, critical=True):
+        # if critical:
+        #     return self._send_recv_sync(method, args)
         
-        return self._send_recv_async(msg)
+        # return self._send_recv_async(method, args)
 
-    def _send_recv_sync(self, msg):
-        method = msg['method']
-        data = msgpack.packb(msg['args'])
-        self.socket.send_data(method, data)
+    # def _send_recv_sync(self, msg):
+    #     method = msg['method']
+    #     data = msgpack.packb(msg['args'])
+    #     log('send', method)
+    #     self.sender.send_data(method, data)
 
-        while True:
-            method_recv, data_recv = self.socket.recv_data()
-            if method_recv == method:
-                break
+    #     while True:
+    #         log('recv', method)
+    #         method_recv, data_recv = self.receiver.recv_data()
+    #         log('recved', method_recv)
+    #         if method_recv == method:
+    #             break
 
-        return msgpack.unpackb(data_recv)
+    #     return msgpack.unpackb(data_recv)
 
-    def _send_recv_async(self, msg):
-        method = msg['method']
-        args, kwargs = msg['args']
+    def _send_recv_async(self, method, args, critical):
+        args, kwargs = args
 
         tt = TicToc()
         tt.tic()
-        if method['name'] == 'predict':
+        if method == 'predict':
             image = args[0]
             assert isinstance(image, np.ndarray), 'Expected image'
             ret_code, data = cv2.imencode(".jpg", image, [int(cv2.IMWRITE_JPEG_QUALITY), opt.jpg_quality])
@@ -119,25 +117,42 @@ class PredictorRemote:
             data = msgpack.packb((args, kwargs))
         self.timing.add('PACK', tt.toc())
 
+        meta = {
+            'name': method,
+            'critical': critical
+        }
+
         # tt.tic()
         # self.socket.send_data(attr, data)
         # self.timing.add('SEND', tt.toc())
-        try:
-            self.send_queue.put((method, data), timeout=PUT_TIMEOUT)
-        except queue.Full:
-            log('send_queue is full')
+        if critical:
+            log('send', meta)
+            self.send_queue.put((meta, data))
 
-        # tt.tic()
-        # attr_recv, data_recv = self.socket.recv_data()
-        # self.timing.add('RECV', tt.toc())
-        try:
-            method_recv, data_recv = self.recv_queue.get(timeout=0.01)
-        except queue.Empty:
-            log('recv_queue is empty')
-            return None
+            while True:
+                log('recv', meta)
+                meta_recv, data_recv = self.recv_queue.get()
+                log('recved', meta_recv)
+                if meta_recv == meta:
+                    break
+        else:
+            try:
+                # TODO: find good timeout
+                self.send_queue.put((meta, data), timeout=PUT_TIMEOUT)
+            except queue.Full:
+                log('send_queue is full')
+
+            # tt.tic()
+            # attr_recv, data_recv = self.socket.recv_data()
+            # self.timing.add('RECV', tt.toc())
+            try:
+                meta_recv, data_recv = self.recv_queue.get(timeout=0.01)
+            except queue.Empty:
+                log('recv_queue is empty')
+                return None
 
         tt.tic()
-        if method_recv['name'] == 'predict':
+        if meta_recv['name'] == 'predict':
             result = cv2.imdecode(np.frombuffer(data_recv, dtype='uint8'), -1)
         else:
             result = msgpack.unpackb(data_recv)
