2da8b3c3f444ec290ef2d7f69366da3b29927306
Ali Aliev <alievk91@gmail.com>
2020-05-19 15:52:56 +0300

    kill other processes

--- run_mac.sh
+++ run_mac.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+kill -9 $(ps aux | grep 'afy/cam_fomm.py' | awk '{print $2}') 2> /dev/null
+
 source scripts/settings.sh
 
 source $(conda info --base)/etc/profile.d/conda.sh
@@ -8,4 +12,4 @@ CKPT=vox-adv-cpk.pth.tar
 
 export PYTHONPATH=$PYTHONPATH:$(pwd):$(pwd)/fomm
 
-python afy/cam_fomm.py --config $CONFIG --checkpoint $CKPT --cam $CAMID --relative --adapt_scale --no-pad $@
+python afy/cam_fomm.py --config "$CONFIG" --checkpoint "$CKPT" --cam $CAMID --relative --adapt_scale --no-pad $@
