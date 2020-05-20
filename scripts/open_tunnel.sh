e94cbb68589beac9099e8631d0cff36af69211e0
Ali Aliev <alievk91@gmail.com>
2020-05-20 18:03:20 +0300

    fix ngrok installation check

--- scripts/open_tunnel.sh
+++ scripts/open_tunnel.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-command -v ngrok >/dev/null 2>&1
+command -v ./ngrok >/dev/null 2>&1
 if [[ $? -ne 0 ]]; then
-    echo ngrok is not found, installing
+    echo ngrok is not found, installing...
     wget -q -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
     unzip -qq -n ngrok-stable-linux-amd64.zip
+    echo Done!
 fi
 
 kill -9 $(ps aux | grep 'ngrok' | awk '{print $2}') 2> /dev/null
 
+echo Opening tunnel
 ./ngrok start --all --config ngrok.conf
