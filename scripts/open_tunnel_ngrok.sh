b312650da22b5166225f8874039c07d2c3be8cb3
Ali Aliev <alievk91@gmail.com>
2020-05-20 19:31:39 +0300

    don't kill yourself

--- scripts/open_tunnel_ngrok.sh
+++ scripts/open_tunnel_ngrok.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-kill -9 $(ps aux | grep 'ngrok' | awk '{print $2}') 2> /dev/null
+cmd="./ngrok start --all --config ngrok.conf"
+
+kill -9 $(ps aux | grep $cmd | awk '{print $2}') 2> /dev/null
 
 echo Opening tunnel
-./ngrok start --all --config ngrok.conf
+$cmd
+
