218f58ccfded9fe85acdb3e45f528d6e7c1e3ac0
Ali Aliev <alievk91@gmail.com>
2020-05-22 22:35:40 +0300

    remove obsolete params

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -1,15 +1,3 @@
-# Web-camera device id
-# Call `v4l2-ctl --list-devices` and find your web-camera device, e.g. /dev/videoX, where X is camera id
-CAMID=0
-MAC_CAMID=2
-
-if [ "$(uname)" == "Darwin" ]; then
-    CAMID=$MAC_CAMID  # set default CAMID if Mac
-fi
-
-# ################################################
-# Normally you don't want to change settings below
-
 # [Linux] Virtual camera device
 # Make sure this id is greater than maximum device id in the list `v4l2-ctl --list-devices`
 # Don't set a big number, it's known that Zoom does not detect cameras with id like 99
