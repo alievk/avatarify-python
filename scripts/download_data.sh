fdf6c23cb32e37f35bc4c448523effea4c732308
George Korepanov <gkorepanov.gk@gmail.com>
2023-08-02 20:51:49 +0300

    Update s3 link in download data script

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -2,7 +2,7 @@
 
 filename=vox-adv-cpk.pth.tar
 
-curl https://openavatarify.s3.amazonaws.com/weights/$filename -o $filename
+curl https://openavatarify.s3-avatarify.com/weights/$filename -o $filename
 
 echo "Expected checksum: 8a45a24037871c045fbb8a6a8aa95ebc"
 
